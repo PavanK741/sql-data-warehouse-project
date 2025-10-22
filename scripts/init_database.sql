9bf94975fbd9f8d4866d08bb114c8be019af7332
PavanK741 <77979048+PavanK741@users.noreply.github.com>
2025-10-22 16:37:53 -0400

    Comment out database drop commands in init script
    Comment out the commands for dropping the 'DataWarehouse' database to prevent accidental deletion.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -14,16 +14,16 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
-USE master;
-GO
+-- USE master;
+-- GO
 
 -- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
-GO
+-- IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+-- BEGIN
+--     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+--     DROP DATABASE DataWarehouse;
+-- END;
+-- GO
 
 -- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
